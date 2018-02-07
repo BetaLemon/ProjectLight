b555e6d2ff0ba412dc26d5849f94d0481c7f044c
Unknown <alexwelo@posteo.net>
2018-02-07 20:20:08 +0100

    Commented CameraScript

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
-    public GameObject player;
-    private Vector3 offset;
-    public float speed = 4.0f;
+    // This script controls camera movement
+
+    public GameObject player;   // Stores the player, to use his position later on.
+    private Vector3 offset;     // Vector that stores the offset between the player and the camera.
+    public float speed = 4.0f;  // Speed at which the camera lerps.
 
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
+        offset = transform.position - player.transform.position;    // The offset is read at start. Might be changed at some point in time.
 	}
 
     // LateUpdate is called after Update each frame
     void FixedUpdate () {
 
-        float interpolation = speed * Time.deltaTime;
+        float interpolation = speed * Time.deltaTime;   // Calculates the interpolation for a smooth lerp.
 
-        Vector3 position = transform.position;
+        Vector3 position = transform.position;  // stores the camera's position.
+        // We change the camera's coordinates following the player's position and applying interpolation:
         position.y = Mathf.Lerp(transform.position.y, player.transform.position.y + offset.y, interpolation);
         position.x = Mathf.Lerp(transform.position.x, player.transform.position.x + offset.x, interpolation);
         position.z = Mathf.Lerp(transform.position.z, player.transform.position.z + offset.z, interpolation);
 
-        transform.position = position ;
+        transform.position = position ; // The position of the camera is finally modified.
 
         /*
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
