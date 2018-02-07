43285c7e67248332908a3e2a9a0f5621633e5185
Unknown <alexwelo@posteo.net>
2018-02-07 17:29:20 +0100

    Commented and cleaned MovingPlatform and Mirror scripts.

--- Assets/Scripts/Mechanics/Mirror.cs
+++ Assets/Scripts/Mechanics/Mirror.cs
@@ -4,69 +4,79 @@ using UnityEngine;
 
 public class Mirror : MonoBehaviour {
 
-    public GameObject Kamehameha;
+    // Mirror is a script that enables light reflection for mirrors and other objects. To differentiate the
+    // light source from others, it is internally called Kamehameha. It owns such a Kamehameha and points it
+    // in the reflected angles direction. Mirrors can reflect with each other.
 
-    private bool reflecting;
-    private Vector3 incomingVec, normalVec, hitPoint, reflectVec;
+    public GameObject Kamehameha;   // Stores the cylinder that represents the player's light ray. Internally called Kamehameha.
 
-    private RaycastHit rayHit;
-    private bool hitOtherMirror;
-    private bool hitMovingPlatform;
+    private bool reflecting;    // Controls whether it needs to make calculations and show the Kamehameha.
+    // Vectors that store: the incoming light, the normal vector of the mirror, the position at which the light enters and leaves, the direction at which it leaves:
+    private Vector3 incomingVec, normalVec, hitPoint, reflectVec;   
+    
+    private RaycastHit rayHit;          // Saves the hit when the raycast intersects with a collider.
+    private bool hitOtherMirror;        // Stores specifically if a mirror has been hit.
+    private bool hitMovingPlatform;     // Stores specifically if a moving platform was hit.
 
     // Use this for initialization
     void Start () {
+        // We set the initial values for the two hitting bools:
         hitOtherMirror = false;
         hitMovingPlatform = false;
 	}
-
-    public void Reflect(Vector3 inVec, Vector3 normal, Vector3 point)
+    
+    // Function that is called when a raycast has hit our mirror and a reflection is expected:
+    public void Reflect(Vector3 inVec, Vector3 normal, Vector3 point)   // Parameters actually come from the Raycast.
     {
+        // We update our vector to the values of the raycastHit:
         incomingVec = inVec;
         normalVec = normal;
         hitPoint = point;
-        reflecting = true;
+        reflecting = true;  // We are now reflecting!
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (reflecting){
-            Kamehameha.transform.localScale = new Vector3(16, 16, 16);
-            Kamehameha.transform.position = hitPoint;
-            reflectVec= Vector3.Reflect(incomingVec, normalVec);
-            Kamehameha.transform.forward = reflectVec;
-            reflecting = false;
+        if (reflecting){    // If we are reflecting:
+            Kamehameha.transform.localScale = new Vector3(16, 16, 16);  // Set the size of our mirror's Kamehameha to the preliminary defaults: 16,16,16.
+            Kamehameha.transform.position = hitPoint;                   // We set the Kamehameha's position to where the light hit.
+            reflectVec= Vector3.Reflect(incomingVec, normalVec);        // We calculate the reflection vector, using our incoming and normal vectors.
+            Kamehameha.transform.forward = reflectVec;                  // We make the Kamehameha look in the direction of the reflected vector.
+            reflecting = false;                                         // After this execution we won't be reflecting anymore.
 
-            if (Physics.Raycast(hitPoint, reflectVec, out rayHit))
+            if (Physics.Raycast(hitPoint, reflectVec, out rayHit))      // If our casted ray hits something:
             {   
-                Debug.DrawRay(hitPoint, reflectVec * 1000, Color.blue);
-                if (rayHit.collider.gameObject.CompareTag("Mirror")) { OtherMirror(rayHit); hitOtherMirror = true; }
-                else if (rayHit.collider.gameObject.CompareTag("MovingPlatform")) { hitMovingPlatform = true; }
-                else { hitOtherMirror = false; hitMovingPlatform = false; }
+                Debug.DrawRay(hitPoint, reflectVec * 1000, Color.blue);     // For debugging reasons, we display the ray.
+                if (rayHit.collider.gameObject.CompareTag("Mirror")) { OtherMirror(rayHit); hitOtherMirror = true; }    // If we have hit a Mirror -> OtherMirror(). Hit mirror!
+                else if (rayHit.collider.gameObject.CompareTag("MovingPlatform")) { hitMovingPlatform = true; }     // If we hit a moving platform, well we hit it... (?)
+                else { hitOtherMirror = false; hitMovingPlatform = false; }     // Else, we ain't hitting anything, guys.
 
-                if (rayHit.collider.gameObject.CompareTag("Trigger")) { TriggerTrigger(rayHit); }
+                if (rayHit.collider.gameObject.CompareTag("Trigger")) { TriggerTrigger(rayHit); }   // If we hit a Trigger, then we trigger it -> TriggerTrigger().
             }
-            else
+            else   // If our ray didn't hit shit...
             {
+                // ... then, well, nothing was hit:
                 hitOtherMirror = false;
                 hitMovingPlatform = false;
             }
         }
-        else
+        else    // If we're not reflecting:
         {
-            Kamehameha.transform.localScale = new Vector3(0, 0, 0);
+            Kamehameha.transform.localScale = new Vector3(0, 0, 0); // We make the Kamehameha suuuuuuper tiny.
         }
         //transform.Rotate(new Vector3(0,1,0));
 	}
 
+    // Function that is called when another mirror is hit:
     void OtherMirror(RaycastHit mirrorHit)
     {
-        Vector3 inVec = mirrorHit.point - hitPoint;
-        mirrorHit.collider.GetComponentInParent<Mirror>().Reflect(inVec, mirrorHit.normal, mirrorHit.point);
-        Kamehameha.transform.localScale = new Vector3(16, 16, Vector3.Distance(mirrorHit.point, Kamehameha.transform.position) / 2);
+        Vector3 inVec = mirrorHit.point - hitPoint; // The incoming vector for the receiving mirror is the point where we were hit minus the point where it was hit.
+        mirrorHit.collider.GetComponentInParent<Mirror>().Reflect(inVec, mirrorHit.normal, mirrorHit.point);    // We tell that mirror to reflect.
+        Kamehameha.transform.localScale = new Vector3(16, 16, Vector3.Distance(mirrorHit.point, Kamehameha.transform.position) / 2);    // We make Kamehameha the length of the distance.
     }
-
+    // Function that is called when a trigger is hit:
     void TriggerTrigger(RaycastHit rh)
     {
-        rh.collider.gameObject.GetComponentInParent<Trigger>().pleaseTrigger();
+        rh.collider.gameObject.GetComponentInParent<Trigger>().pleaseTrigger(); // Tell the trigger to please trigger. Thanks.
     }
 }
--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
 
-    public GameObject[] positions;
-    public GameObject platform;
+    // The MovingPlatform script controlls a model that behaves like a moving platform. It moves between
+    // the different nodes that are set in Unity's editor.
 
-    private int currentNode;
-    public bool isRunning;
+    public GameObject[] positions;  // Array with all the different positions it moves to. Set in Editor.
+    public GameObject platform;     // Model that will be moved. Needs to have a Trigger Collider to work properly.
 
-    public float speed;
+    private int currentNode;        // The current node the platform is moving towards.
+    public bool isRunning;          // Controls if the platform is moving or not.
+
+    public float speed;             // Speed at which the platform moves.
+
+    /* An option could be added that allows the platform to got forwards and backwards, instead
+     * of traversing the nodes in a cicle.
+     */
 
     // Use this for initialization
     void Start () {
@@ -19,12 +26,15 @@ public class MovingPlatform : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (isRunning)
+        if (isRunning)  // If the platforming is running:
         {
+            // Change the position of the platform to the one of the current node, at speed:
             platform.transform.position = Vector3.MoveTowards(platform.transform.position, positions[currentNode].transform.position, speed);
+            // If the platform has reached currentNode, then go to the next node (modular arithmetics):
             if (Vector3.Equals(platform.transform.position, positions[currentNode].transform.position)) { currentNode = (currentNode + 1) % positions.Length; }
         }
     }
 
-    public void getTriggered() { isRunning = true; }
+    // If a Trigger Object is set to activate this moving platform, the following function will be called:
+    public void getTriggered() { isRunning = true; }    // If it is activated then set running to true.
 }
