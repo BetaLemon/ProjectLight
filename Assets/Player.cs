803a0315cfccb4b6328940835640d1273eb5db49
Unknown <dylancalaflatham@gmail.com>
2018-02-16 18:22:55 +0100

    Player death, World Areas and Lethal Zones

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour {
     public float health; //Current mana/health of the player. Magic and health are the same. If all health is lost, the player dies
     public float maxHealth = 100f; //Maximum playerHealth the player can reach
     private float minHealth = 0f; //Health at which the player dies
+    public float respawnHealth = 15; //Health with which the player respawns after death
+
+    private GameObject currentArea = null; //La ultima area en la que el jugador ha entrado
 
 	// Use this for initialization
 	void Start () {
@@ -16,12 +19,29 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Health limits:
 		if(health > maxHealth) { health = maxHealth; }
-        if(health < minHealth) { health = minHealth; }
+        if(health < minHealth) { health = minHealth; Die(); }
 	}
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("BlackInsect")) { health -= 1*Time.deltaTime; }
+        if (other.gameObject.CompareTag("Area")) { currentArea = other.gameObject; } //Player enters area, we save the area
+        else if (other.gameObject.CompareTag("BlackInsect")) { health -= 1 * Time.deltaTime; } //Black insect's dark areas damage the player
+        else if (other.gameObject.CompareTag("Lethal")) { Die(); } //Player enters lethal area, dies and respawns
+    }
+
+    private void Die() {
+        print("Player died");
+        //Send to spawn point:
+        if (currentArea != null) {
+            //transform.position = currentArea.GetComponentInChildren<GameObject>().transform.position;
+            transform.position = currentArea.transform.GetChild(0).transform.position;
+        }
+        else {
+            transform.position = new Vector3(0,0,0); //Base Spawn
+        }
+        //Reset Health:
+        health = respawnHealth;
     }
 }
