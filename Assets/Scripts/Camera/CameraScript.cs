69be312d0ca12c7f7f5349245c88a84be826cee7
BetaLemon <alexwelo@posteo.net>
2018-03-15 19:38:30 +0100

    Many changes to folders, and LightOrb was modified
    I made it so that you need to pass as a parameter the amount of charge the orb receives, instead of assuming that it's the players healthDrainAmount.

---
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    // This script controls camera movement
+
+    public enum CameraMode { GAME_START, FOCUSED, PLAYER };
+    private CameraMode mode;
+    private GameObject focused;
+
+    public GameObject player;   // Stores the player, to use his position later on.
+    private Vector3 offset;     // Vector that stores the offset between the player and the camera.
+    public float speed = 4.0f;  // Speed at which the camera lerps.
+    
+    public GameObject[] panNodes;
+    public GameObject lookAtPoint;
+    private int currentNode;
+    public float tolerance; //needs to be private.
+
+    // FOR ALPHA ONLY:
+    private bool finishedTransition = false;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;    // The offset is read at start. Might be changed at some point in time.
+        if (panNodes.Length == 0 || lookAtPoint == null) { mode = CameraMode.PLAYER; print("No nodes for camera are set, and/or no look at point set."); }
+        else { mode = CameraMode.GAME_START; }
+        focused = null;
+
+        currentNode = 0;
+
+        if(mode == CameraMode.GAME_START) { transform.position = panNodes[currentNode].transform.position; }
+	}
+
+    // LateUpdate is called after Update each frame
+    void FixedUpdate () {
+
+        //if(focused == null) { mode = CameraMode.PLAYER; }
+
+        switch (mode)
+        {
+            case CameraMode.GAME_START:
+                GameStartCamera();
+                break;
+            case CameraMode.PLAYER:
+                PlayerCamera();
+                break;
+            case CameraMode.FOCUSED:
+                FocusedCamera();
+                break;
+        }
+    }
+
+    void PlayerCamera()
+    {
+        //if(transform.rotation != Quaternion.LookRotation(player.transform.position) && !finishedTransition) { transform.LookAt(player.transform.position); }
+        //else { finishedTransition = true; }
+        transform.LookAt(player.transform.position);
+
+        float interpolation = speed * Time.deltaTime;   // Calculates the interpolation for a smooth lerp.
+
+        Vector3 position = transform.position;  // stores the camera's position.
+                                                // We change the camera's coordinates following the player's position and applying interpolation:
+        position.y = Mathf.Lerp(transform.position.y, player.transform.position.y + offset.y, interpolation);
+        position.x = Mathf.Lerp(transform.position.x, player.transform.position.x + offset.x, interpolation);
+        position.z = Mathf.Lerp(transform.position.z, player.transform.position.z + offset.z, interpolation);
+
+        transform.position = position; // The position of the camera is finally modified.
+
+        /*
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;*/
+    }
+
+    void FocusedCamera()
+    {
+        float interpolation = speed * Time.deltaTime;   // Calculates the interpolation for a smooth lerp.
+
+        Vector3 position = transform.position;  // stores the camera's position.
+                                                // We change the camera's coordinates following the player's position and applying interpolation:
+        position.y = Mathf.Lerp(transform.position.y, focused.transform.position.y + 3, interpolation);
+        position.x = Mathf.Lerp(transform.position.x, focused.transform.position.x, interpolation);
+        position.z = Mathf.Lerp(transform.position.z, focused.transform.position.z - 3, interpolation);
+
+        transform.position = position; // The position of the camera is finally modified.
+    }
+
+    void GameStartCamera()
+    {
+        //Vector3 moveVec, camNextNode, camNextNextNode;
+        //camNextNode = transform.position - panNodes[currentNode].transform.position;
+        //camNextNextNode = transform.position - panNodes[currentNode+1].transform.position;
+        //moveVec = (camNextNode + camNextNextNode) * -1;
+
+        //transform.position = transform.position + (moveVec.normalized * 0.2f);
+
+        //transform.Translate(moveVec.normalized * 10);
+
+        transform.position = Vector3.MoveTowards(transform.position, panNodes[currentNode].transform.position, 0.1f);
+        transform.LookAt(lookAtPoint.transform.position);
+
+        //Vector3 middlePointBetweenNodes = panNodes[currentNode].transform.position + panNodes[currentNode + 1].transform.position;
+        //middlePointBetweenNodes = middlePointBetweenNodes / 2;
+
+        if (Vector3.Distance(transform.position, panNodes[currentNode].transform.position) < tolerance)
+        {
+            currentNode = (currentNode + 1) % panNodes.Length;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) { mode = CameraMode.PLAYER; }
+        //if(Vector3.Distance(transform.position, middlePointBetweenNodes) < tolerance)
+        //{
+        //    currentNode = (currentNode + 1) % panNodes.Length;
+        //}
+    }
+
+    public void setFocus(GameObject target)
+    {
+        if(target == focused)
+        {
+            stopFocus();
+        }
+        else
+        {
+            focused = target;
+            mode = CameraMode.FOCUSED;
+        }
+    }
+
+    public void stopFocus()
+    {
+        focused = null;
+        mode = CameraMode.PLAYER;
+    }
+
+    public CameraMode getMode() { return mode; }
+    public void setMode(CameraMode _mode) { mode = _mode; }
+}
---
+++ Assets/Scripts/Mechanics/Mirror/MirrorRotation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorRotation : MonoBehaviour {
+
+    public Transform[] VerticalAxisRotator;
+    public Transform[] HorizontalAxisRotator;
+    public Transform[] CrankRotator;
+    public Transform[] SideGearRotator;
+
+    public float VerticalAngle;
+    public float HorizontalAngle;
+
+    // Use this for initialization
+    void Start () {
+        VerticalAngle = HorizontalAngle = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < VerticalAxisRotator.Length; i++)
+        {
+            VerticalAxisRotator[i].Rotate(new Vector3(0, 0, VerticalAngle));
+        }
+        for (int i = 0; i < HorizontalAxisRotator.Length; i++)
+        {
+            HorizontalAxisRotator[i].Rotate(new Vector3(HorizontalAngle, 0, 0));
+        }
+        for(int i = 0; i < CrankRotator.Length; i++)
+        {
+            CrankRotator[i].Rotate(new Vector3(VerticalAngle, 0, 0));
+        }
+        for (int i = 0; i < SideGearRotator.Length; i++)
+        {
+            SideGearRotator[i].Rotate(new Vector3(0, 0, -VerticalAngle));
+        }
+    }
+}
