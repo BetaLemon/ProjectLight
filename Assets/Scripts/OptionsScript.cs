33a4e755fd4b638d053977ba51f6039fc1edcdef
Unknown <dylancalaflatham@gmail.com>
2018-03-16 01:45:19 +0100

    Auto stash before merge of "dylan" and "origin/dylan"

---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsScript : MonoBehaviour {
+
+    public GameObject MainMenuRef;
+    public GameObject MainMenuCanvasRef;
+
+    public void GoToMainMenu()
+    {
+        gameObject.SetActive(false);
+        MainMenuRef.SetActive(true);
+        MainMenuCanvasRef.SetActive(true);
+    }
+}
