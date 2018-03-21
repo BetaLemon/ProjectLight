2d61354bf78a361a57d849dfc11fc5b3fc4e6705
Unknown <alexwelo@posteo.net>
2018-03-21 17:19:36 +0100

    Shet

--- Assets/Standard Assets/Environment/Water/Water/Scripts/Displace.cs
+++ Assets/Standard Assets/Environment/Water/Water/Scripts/Displace.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 
@@ -34,41 +33,4 @@ namespace UnityStandardAssets.Water
             Shader.DisableKeyword("WATER_VERTEX_DISPLACEMENT_ON");
         }
     }
-=======
-using System;
-using UnityEngine;
-
-namespace UnityStandardAssets.Water
-{
-    [ExecuteInEditMode]
-    [RequireComponent(typeof(WaterBase))]
-    public class Displace : MonoBehaviour
-    {
-        public void Awake()
-        {
-            if (enabled)
-            {
-                OnEnable();
-            }
-            else
-            {
-                OnDisable();
-            }
-        }
-
-
-        public void OnEnable()
-        {
-            Shader.EnableKeyword("WATER_VERTEX_DISPLACEMENT_ON");
-            Shader.DisableKeyword("WATER_VERTEX_DISPLACEMENT_OFF");
-        }
-
-
-        public void OnDisable()
-        {
-            Shader.EnableKeyword("WATER_VERTEX_DISPLACEMENT_OFF");
-            Shader.DisableKeyword("WATER_VERTEX_DISPLACEMENT_ON");
-        }
-    }
->>>>>>> lemon
 }
--- Assets/Standard Assets/Environment/Water/Water/Scripts/SpecularLighting.cs
+++ Assets/Standard Assets/Environment/Water/Water/Scripts/SpecularLighting.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 
@@ -31,38 +30,4 @@ namespace UnityStandardAssets.Water
             }
         }
     }
-=======
-using System;
-using UnityEngine;
-
-namespace UnityStandardAssets.Water
-{
-    [RequireComponent(typeof(WaterBase))]
-    [ExecuteInEditMode]
-    public class SpecularLighting : MonoBehaviour
-    {
-        public Transform specularLight;
-        private WaterBase m_WaterBase;
-
-
-        public void Start()
-        {
-            m_WaterBase = (WaterBase)gameObject.GetComponent(typeof(WaterBase));
-        }
-
-
-        public void Update()
-        {
-            if (!m_WaterBase)
-            {
-                m_WaterBase = (WaterBase)gameObject.GetComponent(typeof(WaterBase));
-            }
-
-            if (specularLight && m_WaterBase.sharedMaterial)
-            {
-                m_WaterBase.sharedMaterial.SetVector("_WorldLightDir", specularLight.transform.forward);
-            }
-        }
-    }
->>>>>>> lemon
 }
