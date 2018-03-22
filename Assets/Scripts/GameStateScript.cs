76d0cca62064f235aac21d11c20d34ba6f4a0dd8
Unknown <dylancalaflatham@gmail.com>
2018-03-22 22:17:12 +0100

    the commit that should have existed before.

---
+++ Assets/Scripts/GameStateScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script can keep track of stuff globally.
+
+public class GameStateScript : MonoBehaviour {
+    private int sceneState; //1=MAIN MENU, 2=OPTIONS, 3=FILE SELECT, 4=INGAME
+	void Start () {
+        sceneState = 1;
+    }
+
+    //void Update () {
+
+    //}
+
+    public void setSceneState(int state)
+    {
+        sceneState = state;
+    }
+    public int getSceneState()
+    {
+        return sceneState;
+    }
+
+
+}
