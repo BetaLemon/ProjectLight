bc9c166072372440f28d919ff661a72bcdecf1db
Mangonels <dylancalaflatham@gmail.com>
2018-05-23 15:44:32 +0200

    Fmod music stuff & fixes

--- Assets/Scripts/Game Logic/MenuScript.cs
+++ Assets/Scripts/Game Logic/MenuScript.cs
@@ -2,6 +2,9 @@
 
 public class MenuScript : MonoBehaviour {
 
+    [FMODUnity.EventRef]
+    public string clicksound;
+
     public GameStateScript gameStateDataScriptRef; //Reference to the Game/Global World Scene State
 
     void Start() //The following always happens when the main menu appears:
@@ -12,14 +15,18 @@ public class MenuScript : MonoBehaviour {
 
     public void FileSelectionMode()
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/MenuButtonClick", transform.position);
+
         gameStateDataScriptRef.SetSceneState(GameStateScript.SceneState.FILESELECT); //Informs the GameState that we are now heading to file selector
     }
     public void GoToOptions()
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/MenuButtonClick", transform.position);
         gameStateDataScriptRef.SetSceneState(GameStateScript.SceneState.OPTIONS);
     }
     public void Quit()
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/MenuButtonClick", transform.position);
         Application.Quit();
     }
 }
