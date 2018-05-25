63dc7d8a752e7c61ac3a48887829853f426d4c25
BetaLemon <alexwelo@posteo.net>
2018-05-25 18:12:32 +0200

    WIP Tutorial. Added QuickUI camera proximity fade.

---
+++ Assets/TutorialScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour {
+
+    public Transform playerSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.M))
+        {
+            Player.instance.transform.position = playerSpawn.position;
+        }
+	}
+}
