c5fd6423310e9221fecd7d44dc8f23b180f21b84
BetaLemon <alexwelo@gmx.com>
2018-05-26 15:46:35 +0200

    Major advancements with Tutorial
    Works until light expand step.

--- Assets/TutorialScript.cs
+++ Assets/TutorialScript.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class TutorialScript : MonoBehaviour {
 
+    // Steps:
+    enum Steps
+    {
+        WELCOME, SHOW_BASICS, MOVEMENT, LIGHT_INTRO, LIGHT_INTRO2, LIGHT_E, LIGHT_E_DONE
+    }
+
+    #region Variables
     public Transform playerSpawn;
     public string KeyboardInputAxis;
     public string GamepadInputAxis;
 
     public TextMesh text;
-    [TextArea(5, 20)]
+    [TextArea(2, 20)]   // (min, max)
     public List<string> textLines;
     private int currentText;
 
-    private int step;
-    private int prevStep;
+    public Transform PressQ;
+    private bool qState;
 
-	// Use this for initialization
-	void Start () {
-        step = 0; prevStep = -1;
+    private Steps step;
+    private Steps prevStep;
+
+    private float stepDelay = 1f;
+    private float dt;
+
+    private PlayerInput input;
+    public GameObject OrbPrefab;
+    private LightOrb orb;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        step = 0; prevStep = step-1;
         currentText = 0;
+        dt = 0;
+        input = PlayerInput.instance;
 	}
 	
 	// Update is called once per frame
@@ -28,23 +48,61 @@ public class TutorialScript : MonoBehaviour {
 
         if(Input.GetAxis(KeyboardInputAxis) != 0 || Input.GetAxis(GamepadInputAxis) != 0)   // Button was pressed.
         {
-            step++;
+            if (qState && dt > stepDelay) { step++; dt = 0; }
         }
 
-        if (step == prevStep) return;
+        dt += Time.deltaTime;
 
+        //if (step == prevStep) return;
+
+        text.text = textLines[(int)step];   // Text for that step.
+        // Custom behaviour;
         switch (step)
         {
-            case 0:
-                text.text = textLines[0];
+            case Steps.WELCOME:
+                qState = true;
                 break;
-            case 1:
-                text.text = textLines[1];
+            case Steps.SHOW_BASICS:
+                qState = true;
+                break;
+            case Steps.MOVEMENT:
+                qState = false;
+                if(input.isPressed("Horizontal") || input.isPressed("Vertical"))
+                {
+                    if(dt > stepDelay) step++;
+                }
+                break;
+            case Steps.LIGHT_INTRO:
+                qState = true;
+                break;
+            case Steps.LIGHT_INTRO2:
+                qState = true;
+                break;
+            case Steps.LIGHT_E:
+                qState = false;
+                if (orb == null) {
+                    orb = GameObject.Instantiate(OrbPrefab).GetComponent<LightOrb>();
+                    orb.transform.parent = transform;
+                    orb.transform.position = playerSpawn.position;
+                    orb.orbCharge = 0;
+                }
+                if(orb.orbCharge > 2) { if (dt > stepDelay) step++; }
+                break;
+            case Steps.LIGHT_E_DONE:
+                qState = true;
                 break;
             default:
                 Debug.Log("Default."); step = 0; break;
         }
 
+        SetPressQState(qState);
+
         prevStep = step;
 	}
+
+    void SetPressQState(bool isEnabled)
+    {
+        if(PressQ.gameObject.activeSelf != isEnabled)
+            PressQ.gameObject.SetActive(isEnabled);
+    }
 }
